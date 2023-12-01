62ec66f755dcc681351ae8301ffe11879e082818
Coar-DO <143054357+Coar-DO@users.noreply.github.com>
2023-12-01 11:29:53 +0900

    Add files via upload

---
+++ my-auth-app/web/src/js/main.js
@@ -0,0 +1,9 @@
+function submitLoginForm() {
+  // Implement login logic here
+  alert("Login button clicked");
+}
+
+function submitSignupForm() {
+  // Implement signup logic here
+  alert("Sign Up button clicked");
+}
